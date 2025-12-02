fb18ee7337e3d63e16aaa9b305b4d2540b8b9d54
mohammedfaizalCyberfrat <mohammed.faizal@Cyberfrat.com>
2025-12-02 18:32:08 +0530

    add gif in loader

--- src/User-panel/components/CulsightPageLoader.js
+++ src/User-panel/components/CulsightPageLoader.js
@@ -1,16 +1,26 @@
-import { Spin } from 'antd'
-import React from 'react'
-import { LoadingOutlined } from "@ant-design/icons";
+import React from 'react';
+import GIF from "../assests/02176467793810900000000000000000000ffffac1514363c9e40-ezgif.com-video-to-gif-converter.gif"
 
 function CulsightPageLoader() {
   return (
-    <div style={{ display: "bloack", textAlign: "center", padding: "50px" }}>
-      <div style={{ display: "inline-block", padding: "20px", minWidth: "180px", boxShadow: "0px 0px 8px #ccc", borderRadius: "8px" }}>
-        <Spin indicator={<LoadingOutlined spin style={{color:"#e9c70ada"}}/>} size="large" />
-        <h2 style={{ marginTop: "25px", color: "#e9c70ada" }}>CyberFrat</h2>
+    <div style={{ display: "block", textAlign: "center", padding: "50px" }}>
+      <div style={{
+        display: "inline-block",
+        padding: "20px",
+        minWidth: "180px",
+        boxShadow: "0px 0px 8px #ccc",
+        borderRadius: "8px"
+      }}>
+        
+        <img 
+          src={GIF}  
+          alt="loading"
+          style={{ width: "80px", height: "80px" }}
+        />
+
       </div>
     </div>
-  )
+  );
 }
 
 export default CulsightPageLoader;
