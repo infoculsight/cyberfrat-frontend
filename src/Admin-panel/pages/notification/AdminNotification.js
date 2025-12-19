46ee197fb2f74cc2987042105f9f0b6612ae27b3
mohammedfaizalCyberfrat <mohammed.faizal@Cyberfrat.com>
2025-12-19 19:07:46 +0530

    changes

--- src/Admin-panel/pages/notification/AdminNotification.js
+++ src/Admin-panel/pages/notification/AdminNotification.js
@@ -123,7 +123,6 @@ function AdminNotification() {
         {record.notification_type === 'test_submit' && 'Test Submit'}
         {record.notification_type === 'course_expire' && 'Course Expire'}
         {record.notification_type === 'certificate_issued' && 'Certificate Issue'}
-        {record.notification_type === 'certificate_issued' && 'Certificate Issue'}
         {record.notification_type === 'quiz_uploaded' && 'Quiz Uploaded'}
         {record.notification_type === 'livetest_assign' && 'Live Test Assign'}
       </span>,
